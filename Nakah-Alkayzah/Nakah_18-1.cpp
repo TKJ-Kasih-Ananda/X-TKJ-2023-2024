0674a1b617155d144031b6ce28706cec4c6b3fbb
Nakahalkayzah <143372919+Nakahalkayzah@users.noreply.github.com>
2023-09-18 13:13:34 +0700

    Add files via upload

---
+++ Nakah-Alkayzah/Nakah_18-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  string nama;
+  int umur;
+cout<<"Siapa nama kamu?"; 
+cin >> nama;
+cout<< "namaku" <<nama;
+cout<< "berapa umur kamu?/n";
+cin >> umur;
+}
---
+++ Nakah-Alkayzah/tabung_nakah-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  float phi = 3.14;
+    int r,t,v;
+    string dimensi;
+    cout << "masukan jari jari :";
+    cin >> r;
+    cout << "masukan tinggi :";
+    cin >> t;
+    cout << "masukan satuan (mm/cm/m)";
+    cin >> dimensi;
+    cout << endl;
+    v = phi*r*r*t ;
+    cout << "volume tabung =" <<v<< "" <<dimensi<< "3";
+    return 0;
+}
