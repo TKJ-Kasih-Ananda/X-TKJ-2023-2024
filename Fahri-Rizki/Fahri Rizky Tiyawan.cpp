3fa32e231165d028f9b9b844c2c6ef7ded53e017
FahriRizkyTiyawan <143376821+FahriRizkyTiyawan@users.noreply.github.com>
2023-09-18 13:17:30 +0700

    Add files via upload

---
+++ Fahri-Rizki/Fahri Rizky Tiyawan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+string nama;
+int umur;
+    cout << "siapa nama kamu";
+    cin >> nama;
+    cout << "Namaku"<<nama;
+    cout<<"Berapa umur kamu? /n";
+    cin>>umur;
+}     
