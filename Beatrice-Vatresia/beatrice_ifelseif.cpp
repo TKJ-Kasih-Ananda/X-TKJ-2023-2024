1ab5a57d4a19b05c28fd9cd0f382d08f2af4cdd8
beatricevatrecia <143377178+beatricevatrecia@users.noreply.github.com>
2023-09-18 13:39:46 +0700

    Add files via upload

---
+++ Beatrice-Vatresia/beatrice_ifelseif.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    string nama;
+    int kelas;
+     cout<<"hi kamu kelas berapa?";
+     cin>>kelas;
+     cout<<"siapa nama kamu?";
+     cin>>nama;
+     if (kelas==10){
+        cout<<" hi, kamu"<<nama<<"kelas 10 ya!"<<endl;
+      }
+       else if (kelas==11){
+           cout<<"cie dah kelas 11"; 
+       }
+       else{
+          cout<<"waduh gaterdeteksi";
+          }
+     
+}
