2952252367808c848d458381f462a9a7c21ff2b7
Safiraainun <143378094+Safiraainun@users.noreply.github.com>
2023-09-18 13:23:52 +0700

    Add files via upload

---
+++ Safira-Ainun/LuasSegitiga_nama.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int alas;
+   int tinggi;
+    float luas;
+     cout << "Masukkan alas:";
+      cin >> alas;
+     cout <<"Masukkan tinggi:";
+      cin >> tinggi;
+       luas = 0.5*alas*tinggi;
+     cout << "Luas segitiga adalah"<<luas<<endl;
+     return 0;
+}
---
+++ Safira-Ainun/Safira_23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+string nama;
+int umur;
+cout<<"Siapa nama kamu?";
+cin>>nama;
+cout<<"Namaku"<<nama;
+cout<<"Berapa umur kamu? /n";
+cin>>umur;
+}
