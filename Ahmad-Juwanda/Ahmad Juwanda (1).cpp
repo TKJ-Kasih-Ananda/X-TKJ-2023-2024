cfefc252ee3e14258541a9ae3b07da50f5cd122f
AhmdJuwanda <143384308+AhmdJuwanda@users.noreply.github.com>
2023-09-18 13:16:49 +0700

    Add files via upload

---
+++ Ahmad-Juwanda/Ahmad Juwanda (1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main() {
+    string nama ;
+    int umur ;
+    cout << "siapa nama kamu";
+    cin >> nama ;
+    cout << "namaku"<<nama ;
+    cout << "berapa umur kamu";
+    cin >> umur ;
+    } 
---
+++ Ahmad-Juwanda/daysss.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day=7;
+    cout << "day;";
+   
+    cin >> day;
+    switch (day) {
+        case 1:
+            cout << "Minggu";
+            break;
+        case 2:
+            cout << "Senin";
+            break;
+        case 3:
+            cout << "Selasa";
+            break;
+        case 4:
+            cout << "Rabu";
+            break;
+        case 5:
+            cout << "Kamis";
+            break;
+        case 6:
+            cout << "Jumat";
+            break;
+        case 7:
+            cout << "Sabtu";
+            break;
+    }
+
+    return 0;
+}
