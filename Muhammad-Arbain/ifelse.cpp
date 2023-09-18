2d56459b4045db20e14ac31753d40cf66519ba6c
xTkjMuhammadarbain <145320870+xTkjMuhammadarbain@users.noreply.github.com>
2023-09-18 13:25:30 +0700

    Add files via upload

---
+++ Muhammad-Arbain/ifelse.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+string nama;
+int kelas;
+    cout << "Hi kamu kelas berapa?";
+    cin  >> kelas;
+    cout << "siapa nama kamu?";
+    cin  >> nama;
+    if(kelas ==10){
+      cout << "hi,kamu"<<nama<<"kelas 10 ya!"<<endl;
+     }else{
+       else if(kelas ==11){
+      cout << "cie dah kelas 11";
+      }else{
+        else{
+          cout << "wadug gak tertedeksi";
+          {
+            {
+          
+       
+          
+      
+     
---
+++ Muhammad-Arbain/switch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day;
+    cout << "day (1-7): ";
+    cin >> day;
+    switch (day) {
+        case 1:
+            cout << "Minggu";
+            break;
+        case 2:
+            cout << "Senin";
+            break;
+        case 3:
+            cout << "Selasa";
+            break;
+        case 4:
+            cout << "Rabu";
+            break;
+        case 5:
+            cout << "Kamis";
+            break;
+        case 6:
+            cout << "Jumat";
+            break;
+        case 7:
+            cout << "Sabtu";
+            break;
+    }
+
+    return 0;
+}
