1a4c4ef3d573b919e11ed8df6a050c10653b675a
Rafael Adliyansyah Zulian <143361717+RafaelAdliyansyahzulian@users.noreply.github.com>
2023-09-18 14:30:57 +0700

    Add files via upload

---
+++ Rafael-Adliyansyah/Rafael _IfElse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ string nama;
+  int kelas;
+  cout<<"hai,kamu kelas berapa?";
+  cin>>kelas;
+  cout<<"siapa nama kamu?";
+  cin>>nama;
+  if(kelas==10){
+    cout<<"hai,kamu"<<nama<<"kelas 10 ya!"<<endl;
+    }
+    else{
+    cout<<"yah sayang sekali"; }
+}
