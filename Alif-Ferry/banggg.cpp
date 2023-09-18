86e9250a31c655275086c15a52283334105efda1
Alif ferry <143157876+Alifferry@users.noreply.github.com>
2023-09-18 13:17:03 +0700

    Add files via upload

---
+++ Alif-Ferry/banggg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main() {
+    string nama;
+    int umur;
+     cout << "siapa nama kamu? ";
+     cin >> nama;
+     cout << "namaku"<<nama;
+     cout << "berapa umurmu? /n";
+     cin >> umur;
+     
+}
---
+++ Alif-Ferry/luassegitiga.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int alas;
+    int tinggi;
+    float luas;
+    cout << "masukkan alas";
+    cin >> alas;
+    cout << " masukkan tinggi";
+    cin >> tinggi;
+    luas = 0.5*alas* tinggi;
+    cout << "luas segitiga adalah"<<luas<<endl;
+    return 0;
+}
