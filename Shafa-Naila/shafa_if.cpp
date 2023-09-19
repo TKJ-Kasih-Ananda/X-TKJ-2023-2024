e7de1d64318978538c81ad0a8399ab5fcb7c921b
shafanayy <145321166+shafanayy@users.noreply.github.com>
2023-09-19 08:58:53 +0700

    Add files via upload

---
+++ Shafa-Naila/shafa_if.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    string nama;
+    int kelas;
+    cout << "hi, kamu kelas berapa?:D";
+    cin >> kelas;
+    cout << "siapa nama kamu?:D";
+    cin >> nama;
+    if (kelas==10) {
+      cout << "hi,kamu"<<nama<<"kelas 10 ya!:D"<<endl;
+      }
+      cout <<"yahh sayang sekalii:P";
+}
