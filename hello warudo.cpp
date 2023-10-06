663203942adcba9513c59fa31af21cda55937930
adrianabdiputra <143362132+adrianabdiputra@users.noreply.github.com>
2023-10-06 11:26:18 +0700

    Add files via upload

---
+++ hello warudo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Hello, Dcoder!";
+    cout << " Nama : Adrian Abdi Putra";
+    cout << " Umur : 15";
+}
