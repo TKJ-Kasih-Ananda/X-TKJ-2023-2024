4441615b99553cf4b288161d0fa769427d9f4b76
adrianabdiputra <143362132+adrianabdiputra@users.noreply.github.com>
2023-10-06 11:27:59 +0700

    Add files via upload

---
+++ Adrian-Abdi/Volume Tabung.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int r,t,v;
+    float phi =3.14;
+    string dimensi;
+    cout<<"masukan jari-jari:";
+     cin>> r;
+    cout<<"masukan tinggi:";
+     cin>>t;
+    cout<< "masukan satuan (mm/cm/m):";
+     cin>>dimensi;
+    cout<< endl;
+    v = phi*r*r*t;
+    cout<< "volume tabung="<<v<<"" <<dimensi<<"3";
+    return 0;
+}
