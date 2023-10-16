355320b339ffe2d4e10e560d1aa736ba6919186e
Leopardkent <143376913+Leopardkent@users.noreply.github.com>
2023-10-16 11:57:43 +0700

    Add files via upload

---
+++ Leopard-Kent/LeopardKent_15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+string nama;
+int umur;
+cout <<"siapa nama kamu?";
+cin >>nama;
+cout <<"leopard kent"<<nama;
+cout <<"berapa umur kamu?/n";
+cin>>umur;
+}
