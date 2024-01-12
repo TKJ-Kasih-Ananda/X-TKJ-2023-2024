fffdf53cea0d990e1f9d17e5a4ce1c5cd95103be
Leopardkent <143376913+Leopardkent@users.noreply.github.com>
2024-01-12 11:53:06 +0700

    Add files via upload

---
+++ Leopard-Kent/segitiga_Leopard Kent.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main a,b,c;
+cout << "masukan nilai a"<<endl;
+cin >> a;
+cout << "masukan nilai b"<<endl;
+cin >> b;
+cout << " masukan nilai c"<<endl;
+cin >> c;
+}
