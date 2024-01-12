2c63a0d31b1fc75e9f5c280dba9f551c52046085
Nakahalkayzah <143372919+Nakahalkayzah@users.noreply.github.com>
2024-01-12 11:33:03 +0700

    Add files via upload

---
+++ Nakah-Alkayzah/Nakah_for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int total;
+cout<<"total iteration:";
+cin>>total;
+for (int i=0;i<total; i++)
+cout<<"I'm smart\n";
+getch();
+return 0;
+}
