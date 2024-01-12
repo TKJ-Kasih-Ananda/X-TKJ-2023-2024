2c5394f921d28bd1ed0a3aed63a45021229c61f5
Alif ferry <143157876+Alifferry@users.noreply.github.com>
2024-01-12 11:55:37 +0700

    Add files via upload

---
+++ Alif-Ferry/Alif ferry-For.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+     int  total;
+     cout <<"total iteration";
+     cin >>total;
+     for (int i=0;i<total; i++)
+        cout <<"i'am spiderman/n";
+     getch();
+     return 0;
+}
