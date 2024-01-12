cae912b6e06e10bfdc38273c6ab3fb0a17a90e87
AndiRachman <143360025+AndiRachman@users.noreply.github.com>
2024-01-12 11:30:02 +0700

    Add files via upload

---
+++ Andi-Rachman/Andi_For.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+     int  total;
+     cout <<"total iteration";
+     cin >>total;
+     for (int i=0;i<total; i++)
+        cout <<"i'am smart\n";
+     getch();
+     return 0;
+} 
