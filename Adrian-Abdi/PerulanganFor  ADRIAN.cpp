3c3e2254b4ca442c65cd52e9ffc5e43232ba9f77
adrianabdiputra <143362132+adrianabdiputra@users.noreply.github.com>
2024-01-12 11:40:49 +0700

    Add files via upload

---
+++ Adrian-Abdi/PerulanganFor  ADRIAN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int total;
+cout<<"total iteration:";
+cin>>total;
+{for (int i=0;i<total; i++)
+cout<<"I'm smart\n";
+}
+getch();
+return 0;
+}
