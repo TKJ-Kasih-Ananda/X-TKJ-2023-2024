4638143173991a83389e49c65d15c69a0e5d7728
xTkjMuhammadarbain <145320870+xTkjMuhammadarbain@users.noreply.github.com>
2024-01-12 11:35:47 +0700

    Add files via upload

---
+++ Muhammad-Arbain/looping for do_arbain.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int total;
+
+    cout << "Total iteration: ";
+    cin >> total;
+
+    for (int i = 0; i < total; i++)
+        cout << "aku kaya";
+
+    getch();  
+
+    return 0;
+}
