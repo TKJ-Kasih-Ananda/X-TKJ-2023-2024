86d36e757ce121a7a343a5f77302281b601638f6
sevvaaa <143346305+sevvaaa@users.noreply.github.com>
2024-01-12 11:37:17 +0700

    Add files via upload

---
+++ Sheva-Nazumi/sheva_for (1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+  int total;
+  cout << " total iteration:";
+  
+  cin >> total;
+  
+    for (int i = 0; i < total; i++) {
+    
+  cout<< "i'm smart\n";
+
+    }
+    getch ();
+    return 0;
+}
