c031b68bdb35edd5db4b544da25973fd54069a00
QanitaChayyiraKurniawan <143373032+QanitaChayyiraKurniawan@users.noreply.github.com>
2024-01-12 11:40:27 +0700

    Add files via upload

---
+++ Qanita-Chaira/Ayyira_For.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+int total;
+
+cout<<"total iteration:";
+
+cin>>total;
+
+for (int i = 0 ;i<total; i++)
+
+cout<<"I'm smart\n";
+
+
+getch();
+
+return 0;
+
+}
