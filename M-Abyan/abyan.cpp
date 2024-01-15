7e186532e2e85c7e7c1dc25d33a0f9ac44023083
abyanzufarrrr <143376786+abyanzufarrrr@users.noreply.github.com>
2024-01-15 12:55:46 +0700

    Add files via upload

---
+++ M-Abyan/abyan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int total;
+cout<<"total iteration:";
+cin>>total;
+{for (int i=0;i<total; i++)
+cout<<"saya abyan"<<endl;
+}
+getch();
+return 0;
+}
