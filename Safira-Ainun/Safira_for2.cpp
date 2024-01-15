926b887fed0971c9b0bf057644aa2a56d3c31d1b
Safiraainun <143378094+Safiraainun@users.noreply.github.com>
2024-01-15 10:31:54 +0700

    Add files via upload

---
+++ Safira-Ainun/Safira_for2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int total;
+cout<<"total iteration:";
+cin>>total;
+{for (int i=0;i<total; i++)
+cout<<"bright ganteng"<<endl;
+}
+getch();
+return 0;
+}
