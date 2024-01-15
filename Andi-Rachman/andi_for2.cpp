0e6a2c4ba6130df6f95fe26f1c59b160bac069d9
AndiRachman <143360025+AndiRachman@users.noreply.github.com>
2024-01-15 10:07:55 +0700

    Add files via upload

---
+++ Andi-Rachman/andi_for2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << "i'am andi "<<endl;
+        ++i;
+    } while (i <= 5);
+
+    return 0;
+    }
