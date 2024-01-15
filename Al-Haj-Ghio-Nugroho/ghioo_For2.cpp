4ba5ee8f34fda7a3edce6b54e692fcb17bd6d613
AlhajGioo <143377155+AlhajGioo@users.noreply.github.com>
2024-01-15 09:56:45 +0700

    Add files via upload

---
+++ Al-Haj-Ghio-Nugroho/ghioo_For2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+     int  hasil;
+     cout <<"hasil interaksi";
+     cin >>hasil;
+     for (int i=5;i<10; i++)
+        cout <<"i'am jioo/n"<<endl;
+     getch();
+     return 0;
+}
