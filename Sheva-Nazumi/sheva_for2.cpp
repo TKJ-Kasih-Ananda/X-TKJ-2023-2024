658600df46d0d5b06dbad7dfced3b0ddd206ca29
sevvaaa <143346305+sevvaaa@users.noreply.github.com>
2024-01-15 10:05:44 +0700

    Add files via upload

---
+++ Sheva-Nazumi/sheva_for2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+{
+  int total;
+  cout << "total interation:";
+  cin >> total;
+    for (int i = 0; i<total; i++ ) {
+      cout << i << " get well soon bu hasna :)\n";
+      }
+      
+      getch();
+    return 0;
+}
