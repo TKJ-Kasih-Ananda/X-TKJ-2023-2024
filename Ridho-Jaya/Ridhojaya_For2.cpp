cbcfa1d954910c7187bdcba8ed5123e8f033217d
Ridho <143157206+RIDHOJAYA@users.noreply.github.com>
2024-01-15 10:41:57 +0700

    Add files via upload

---
+++ Ridho-Jaya/Ridhojaya_For2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Untuk mengulang print nomer dari 1 sampai from 50
+    for (int i = 1; i <= 50; ++i) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
