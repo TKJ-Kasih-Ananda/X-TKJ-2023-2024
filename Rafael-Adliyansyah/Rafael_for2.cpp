6df0a66f98778556ff88a9cfcde81e9fd5d910ad
Rafael Adliyansyah Zulian <143361717+RafaelAdliyansyahzulian@users.noreply.github.com>
2024-01-15 12:19:32 +0700

    Add files via upload

---
+++ Rafael-Adliyansyah/Rafael_for2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Contoh for loop untuk mencetak angka 1 hingga 5
+    for (int i = 1; i <= 5; i++) {
+        cout << "Nilai i: " << i << endl;
+    }
+
+    // Contoh for loop untuk mencetak angka 10 hingga 1 dengan langkah mundur
+    for (int j = 10; j >= 1; j--) {
+        cout << "Nilai j: " << j << endl;
+    }
+
+    return 0;
+}
