419d643e7c9a2c8c9307425cb8ae98c691935102
Ridho <143157206+RIDHOJAYA@users.noreply.github.com>
2024-01-19 11:37:44 +0700

    Ridhojaya_For2.cpp

--- Ridho-Jaya/Ridhojaya_For2.cpp
+++ Ridho-Jaya/Ridhojaya_For2.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    // Untuk mengulang print nomer dari 1 sampai from 50
-    for (int i = 1; i <= 50; ++i) {
-        std::cout << i << " ";
+    // Loop pertama untuk variabel x
+    for (int x = 0; x < 3; x++) {
+        // Loop kedua untuk variabel y
+        for (int y = 0; y < 3; y++) {
+            // Menampilkan nilai x dan y ke layar
+            cout << "x = " << x << ", y = " << y << endl;
+        }
     }
 
     return 0;
