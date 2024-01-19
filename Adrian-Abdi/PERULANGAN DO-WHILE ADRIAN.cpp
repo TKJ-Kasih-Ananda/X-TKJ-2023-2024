b37b7ec8d06ff70a47ad769c83a498a72f5aeba4
adrianabdiputra <143362132+adrianabdiputra@users.noreply.github.com>
2024-01-19 22:57:29 +0700

    Add files via upload

---
+++ Adrian-Abdi/PERULANGAN DO-WHILE ADRIAN.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    
+    // perulangan do-while dari 1 sampai 22 
+    cout << i << " "; {
+         i++;
+            while (i <=22);
+    }
+          
+    
+    
+    return 0;
+ }
+    
---
+++ Adrian-Abdi/PERULANGAN WHILE-DO ADRIAN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    
+    // perulangan while dari 1 sampai 22 
+    while (i <=22) {
+          cout << i << " ";
+          ++i;
+    }
+    
+    return 0;
+ }
+    
