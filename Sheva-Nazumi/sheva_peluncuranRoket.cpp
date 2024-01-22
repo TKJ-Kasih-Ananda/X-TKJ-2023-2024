b697e80112080df308a18d9374484d4064b171a7
sevvaaa <143346305+sevvaaa@users.noreply.github.com>
2024-01-22 12:52:19 +0700

    Add files via upload

---
+++ Sheva-Nazumi/sheva_peluncuranRoket.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "Hitung mundur peluncuran roket "<<endl;
+    for (i=10;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "GO!";
+    return 0;
+}
