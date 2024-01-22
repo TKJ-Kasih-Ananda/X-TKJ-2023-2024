c8b45334603dd08f90a2bd1abdf31c2baa831260
Delyciaaurora <143376613+Delyciaaurora@users.noreply.github.com>
2024-01-22 22:19:02 +0700

    Add files via upload

---
+++ Delycia-Cahya-Aurora/delycia_peluncurroket.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int count = 11; count >= 0; count--) {
+        std::cout << count << std::endl;
+    }
+
+    std::cout << "Roket meluncur dalam 3... 2... 1... Meluncur!" << std::endl;
+
+    return 0;
+}
