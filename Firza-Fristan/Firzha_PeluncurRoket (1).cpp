96eeae10a79d70f18bb4897031531a1f0d603197
FirzhaFirstan04 <143377045+FirzhaFirstan04@users.noreply.github.com>
2024-01-22 13:04:25 +0700

    Add files via upload

---
+++ Firza-Fristan/Firzha_PeluncurRoket (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "Hitung mundur peluncuran roket\n";
+    for (i=10;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "START!";
+    return 0;
+}
