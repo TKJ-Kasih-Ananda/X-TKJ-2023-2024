a743f1351d1e8d12fae2e2eb1785fa9520ef7846
Rafael Adliyansyah Zulian <143361717+RafaelAdliyansyahzulian@users.noreply.github.com>
2024-01-24 17:03:03 +0700

    Add files via upload

---
+++ Rafael-Adliyansyah/Rafael_peluncuranRoket.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Deklarasi
+    int i;
+
+    // Algoritma
+    for (i = 60; i >= 0; i--) {
+        cout << i << endl;
+    }
+
+    // Roket meluncur pada hitungan 0
+    cout << "Roket meluncur pada hitungan 0!" << endl;
+
+    return 0;
+}
