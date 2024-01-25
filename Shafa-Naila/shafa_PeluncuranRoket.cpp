45f17ba66f201cbc8ea932a524dc61ac60672b0a
shafaanay <143377845+shafaanay@users.noreply.github.com>
2024-01-25 22:27:55 +0700

    Add files via upload

---
+++ Shafa-Naila/shafa_PeluncuranRoket.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "Hitung mundur peluncuran roket "<<endl;
+    for (i=5;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "MELUNCUR!";
+    return 0;
+}
