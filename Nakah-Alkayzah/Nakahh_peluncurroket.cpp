6351dd5f810d0f37610a9da30e685599d2cad4fc
Nakahalkayzah <143372919+Nakahalkayzah@users.noreply.github.com>
2024-01-25 21:51:13 +0700

    Add files via upload

---
+++ Nakah-Alkayzah/Nakahh_peluncurroket.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    cout<<"hitungan mundur peluncuran roket"<<endl;
+    for (i=60; i>=0; i--) 
+    {
+     cout<<i<<endl;
+    }
+    cout << "roket berhasil diluncurkan";
+    return 0;
+}
