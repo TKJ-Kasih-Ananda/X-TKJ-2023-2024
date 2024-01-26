663ae8a21412022fb9b9a9b54503a95a804ac77b
Safiraainun <143378094+Safiraainun@users.noreply.github.com>
2024-01-26 10:27:31 +0700

    Add files via upload

---
+++ Safira-Ainun/Peluncuranroket_safira.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "peluncuran pesawat akan mulai "<<endl;
+    for (i=10;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "Ayo!";
+    return 0;
+}
