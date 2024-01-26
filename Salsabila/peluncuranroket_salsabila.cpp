2bb4226b6fd4fb236eaadbe4d3e35a4ba7427ab0
Salsabila <143377963+Salsabila452416@users.noreply.github.com>
2024-01-26 10:48:13 +0700

    Add files via upload

---
+++ Salsabila/peluncuranroket_salsabila.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "hitung mundur peluncuran helikopter"<<endl;
+    for (i=10;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "ayoo mulai!";
+    return 0;
+}
