02d2abc743e7470fbf7a426c3bb2a6ab4703041e
Leopardkent <143376913+Leopardkent@users.noreply.github.com>
2024-01-26 10:30:45 +0700

    Add files via upload

---
+++ Leopard-Kent/leopardkent_peluncuran rocket.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Deklarasi
+  int i;
+    // Algoritma
+    //  for (i = 30; i >= 0; i--) {
+cout << i << endl;
+    }
+    // Roket meluncur pada hitungan 0
+   cout << "Roket meluncur pada hitungan 0!" << endl;
+return 0;
+}
