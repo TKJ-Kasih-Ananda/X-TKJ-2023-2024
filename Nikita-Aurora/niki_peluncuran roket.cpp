7f18dc94b4029695a7ddcd6b8834772ae06866db
auroraniki <157342336+auroraniki@users.noreply.github.com>
2024-01-26 10:41:22 +0700

    Add files via upload

---
+++ Nikita-Aurora/niki_peluncuran roket.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "Hitung mundur peluncuran roket "<<endl;
+    for (i=20;i>=0;i--) {
+      cout << i << endl;
+    }
+    cout << "GO!";
+    return 0;
+}
