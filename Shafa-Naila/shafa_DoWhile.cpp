720d42cd21f24d84c6a17d0b92970c6079a6ba00
NurulFikr1 <143380019+NurulFikr1@users.noreply.github.com>
2024-01-29 12:17:59 +0700

    Add files via upload

--- Shafa-Naila/shafa_DoWhile.cpp
+++ Shafa-Naila/shafa_DoWhile.cpp
@@ -1,22 +1,20 @@
-#include <iostream> 
+#include <conio.h>
+#include <iostream>
 using namespace std;
+int main() {
 
-int main()
-{
-   int N; // Deklarasi variabel untuk menyimpan jumlah N
-   int i; // Deklarasi variabel iterasi i
-   int sum; // Deklarasi variabel sum untuk menyimpan hasil penjumlahan
+  int n=1;
+  float jum=0;
 
-   cout << "How many N do you want? "; // Menanyakan jumlah N
-   cin >> N; // Membaca input pengguna
-   sum = 0; // Menginisialisasi sum menjadi 0
+  do {
+        cout<<""<<n;
+        cout<<", ";
+        jum+=n;
+        n+=3;
+     } while (n<=10);
 
-   // Menggunakan do-while loop
-   i = 1; // Menginisialisasi i menjadi 1
-   do {
-     sum = sum + i; // Menambahkan nilai saat ini
-     i++; // Menaikkan nilai i pada loop
-   } while (i <= N); // Melakukan loop 
-
-   cout << "The number of list = " << sum << endl; // menampilkan hasil penjumlahan
+  cout<<""<<endl;
+  cout<<""<<endl;;
+  cout<<"Jumlah : "<<jum<<endl;;
+  getch();
 }
