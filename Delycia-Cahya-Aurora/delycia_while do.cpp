7a13d3e4d14064cf398e2a56ff2bf002933647ea
Delyciaaurora <143376613+Delyciaaurora@users.noreply.github.com>
2024-01-29 12:10:15 +0700

    Add files via upload

---
+++ Delycia-Cahya-Aurora/delycia_while do.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main() {
+
+	int i=1;
+	while (i<=27)
+	{
+	    cout<<"Belajar Perulangan While "<<i<<endl;
+	    i++;	
+	}
+	getch();
+}
