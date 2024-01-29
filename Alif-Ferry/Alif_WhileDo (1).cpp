69a6f386e96c64279b268a3d900cfaff70299e65
Alif ferry <143157876+Alifferry@users.noreply.github.com>
2024-01-29 12:20:33 +0700

    Add files via upload

---
+++ Alif-Ferry/Alif_WhileDo (1).cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main() {
+
+  int i=1;
+  while (i<=10)
+  
+  {
+    cout<<"alif "<<i<<endl;
+      i++;  
+      
+  }
+  getch();
+}
