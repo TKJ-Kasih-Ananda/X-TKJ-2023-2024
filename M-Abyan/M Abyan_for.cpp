3cba5c16876f9c9ef36eca2d88f2e9cc3061b4fb
abyanzufarrrr <143376786+abyanzufarrrr@users.noreply.github.com>
2024-01-29 12:43:15 +0700

    Add files via upload

---
+++ M-Abyan/M Abyan_for.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"We will print numbers from 1 to 20 using for loop"<<endl; for(int i=1; i<=20; i++)
+{
+cout<<"The number is:"<<i<<endl;
+}
+return 0;
+}
